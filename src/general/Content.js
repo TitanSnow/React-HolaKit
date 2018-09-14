0a586ec935b7cbee893cd526792f2b797298c003
TitanSnow <sweeto@live.cn>
2018-09-14 21:08:18 +0800

    add Content

---
+++ src/general/Content.js
@@ -0,0 +1,29 @@
+import styled, { css } from 'react-emotion'
+
+export const style = () => css`
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0.7em 0;
+  }
+  pre {
+    overflow-x: auto;
+    background-color: rgba(0, 0, 0, 0.05);
+    padding: 1rem;
+    code {
+      font-size: 1rem;
+      white-space: pre;
+      overflow-wrap: normal;
+    }
+  }
+  code {
+    overflow-wrap: break-word;
+  }
+`
+
+export default styled.div`
+  ${style};
+`
--- src/general/index.js
+++ src/general/index.js
@@ -1,3 +1,4 @@
 export { default as Root } from './Root'
 export { default as Columns } from './Columns'
 export { default as ColumnsItem } from './ColumnsItem'
+export { default as Content } from './Content'
