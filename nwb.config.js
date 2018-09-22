d85607ce5e73bfcb30773a0cb264e423510113fc
TitanSnow <sweeto@live.cn>
2018-09-22 09:14:05 +0800

    update config

--- nwb.config.js
+++ nwb.config.js
@@ -2,9 +2,15 @@ module.exports = {
   type: 'react-component',
   npm: {
     esModules: true,
-    umd: false
+    umd: false,
   },
   babel: {
-    plugins: 'babel-plugin-emotion'
-  }
+    plugins: 'babel-plugin-emotion',
+    env: {
+      targets: {
+        browsers: 'firefox >= 57, chrome >= 63, opera >= 50, safari >= 11.1',
+      },
+    },
+    stage: 3,
+  },
 }
